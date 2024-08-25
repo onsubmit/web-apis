8086ea24dae5c068feacabb081058bd553927c7f
Andy <onsubmit@outlook.com>
2024-08-25 16:55:29 -0700

    Add colors for warnings and errors

--- src/components/CodeExecutor.tsx
+++ src/components/CodeExecutor.tsx
@@ -1,8 +1,14 @@
-import { useId, useRef, useState } from "react";
+import { useId, useRef } from "react";
 import styles from "./CodeExecutor.module.css";
 
 type CodeExecutorProps = {
   script: string;
+};
+
+const consoleMethodClassMap: Record<string, string> = {
+  log: "",
+  warn: styles.warn,
+  error: styles.error,
 };
 
 function CodeExecutor({ script }: CodeExecutorProps) {
@@ -11,7 +17,7 @@ function CodeExecutor({ script }: CodeExecutorProps) {
 
   function runCode() {
     if (listRef.current) {
-      listRef.current.innerHTML = '';
+      listRef.current.innerHTML = "";
     }
 
     executeScript(script, listId);
@@ -25,31 +31,35 @@ function CodeExecutor({ script }: CodeExecutorProps) {
   );
 }
 
-function replaceConsoleLogs(script: string, listId: string) {
+function replaceConsoleMethods(script: string, listId: string) {
+  function replacer(_match: string, method: string, message: string) {
+    const consoleClass = consoleMethodClassMap[method];
+    return `
+li = document.createElement("li");
+li.textContent = ${message};
+${consoleClass ? `li.classList.add("${consoleMethodClassMap[method]}");` : ""}
+list.appendChild(li);
+`;
+  }
+
   return (
     `const list = document.getElementById('${listId}');\n` +
     `let li = null;\n` +
-    script.replaceAll(
-      /console.log\((?<LOG>.+?)\);/g,
-      `
-li = document.createElement("li");
-li.textContent = $<LOG>;
-list.appendChild(li);
-`
-    )
+    script.replaceAll(/console.(?<METHOD>log|warn|error)\((?<LOG>.+?)\);/g, replacer)
   );
 }
 
 function wrapInTryCatch(script: string) {
-  return `try { ${script} } catch (e) { console.log(e); }`;
+  return `try { ${script} } catch (e) { console.error(e); }`;
 }
 
 function executeScript(script: string, listId: string) {
-  eval(getExecutableScript(script, listId));
+  const executableScript = getExecutableScript(script, listId);
+  eval(executableScript);
 }
 
 function getExecutableScript(script: string, listId: string): string {
-  return `(async () => { ${replaceConsoleLogs(wrapInTryCatch(script), listId)} })()`;
+  return `(async () => { ${replaceConsoleMethods(wrapInTryCatch(script), listId)} })()`;
 }
 
 export { CodeExecutor };
