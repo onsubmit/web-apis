abc2144b421d59763b4e1959f17d51596e1ae9f9
Andy <onsubmit@outlook.com>
2024-08-25 18:40:53 -0700

    Make code editable

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -5,13 +5,15 @@ import CodeMirror from "@uiw/react-codemirror";
 type CodeEditorProps = {
   script: string;
   theme: "light" | "dark";
+  onChange: (newValue: string) => void;
 };
 
-export default function CodeEditor({ script, theme }: CodeEditorProps) {
+export default function CodeEditor({ script, theme, onChange }: CodeEditorProps) {
   return (
     <CodeMirror
       theme={theme === "light" ? vscodeLight : vscodeDark}
       value={script.trim()}
+      onChange={onChange}
       extensions={[javascript()]}
       basicSetup={{
         lineNumbers: false,
--- src/components/Playground.tsx
+++ src/components/Playground.tsx
@@ -1,4 +1,5 @@
 import classNames from "classnames";
+import { useCallback, useState } from "react";
 import CodeEditor from "./CodeEditor";
 import { CodeExecutor } from "./CodeExecutor";
 import styles from "./Playground.module.css";
@@ -9,10 +10,16 @@ type PlaygroundProps = {
 };
 
 export default function Playground({ script, theme }: PlaygroundProps) {
+  const [value, setValue] = useState(script.trim());
+
+  const onScriptChange = useCallback((newValue: string) => {
+    setValue(newValue.trim());
+  }, []);
+
   return (
     <div className={classNames("not-content", styles.className)}>
-      <CodeEditor theme={theme} script={script} />
-      <CodeExecutor script={script} />
+      <CodeEditor theme={theme} script={script} onChange={onScriptChange} />
+      <CodeExecutor script={value} />
     </div>
   );
 }
