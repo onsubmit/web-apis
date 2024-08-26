b2fbd60ad9c5aa64b379cecf43b2e647680619b8
Andy <onsubmit@outlook.com>
2024-08-25 19:04:43 -0700

    Improve editor styles

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,4 +1,5 @@
 import { javascript } from "@codemirror/lang-javascript";
+import { EditorView } from "@codemirror/view";
 import { vscodeDark, vscodeLight } from "@uiw/codemirror-theme-vscode";
 import CodeMirror from "@uiw/react-codemirror";
 
@@ -14,10 +15,11 @@ export default function CodeEditor({ script, theme, onChange }: CodeEditorProps)
       theme={theme === "light" ? vscodeLight : vscodeDark}
       value={script.trim()}
       onChange={onChange}
-      extensions={[javascript()]}
+      extensions={[javascript(), EditorView.lineWrapping]}
       basicSetup={{
         lineNumbers: false,
         foldGutter: false,
+        highlightSelectionMatches: true,
       }}
     />
   );
