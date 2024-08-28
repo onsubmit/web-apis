079eb79c3e71c55ebc6059f0058bbd595d96439b
Andy <onsubmit@outlook.com>
2024-08-27 21:57:16 -0700

    Add BroadcastChannel API

---
+++ src/content/snippets/Broadcast Channel API/receiver2.js
@@ -0,0 +1,22 @@
+if (!window.BroadcastChannel) {
+  console.error("BroadcastChannel not supported in this browser.");
+  return;
+}
+
+// ___Begin visible code snippet___
+
+console.log("Receiver 2: listening for messages...");
+
+const channel = new BroadcastChannel("my-channel");
+
+channel.addEventListener("message", (event) => {
+  console.log("Receiver 2: Message received");
+  console.debug(JSON.stringify(getMessageEvent(event), null, 2));
+});
+
+function getMessageEvent(event) {
+  return {
+    data: event.data,
+    origin: event.origin,
+  };
+}
---
+++ src/content/snippets/Broadcast Channel API/sender.js
@@ -0,0 +1,24 @@
+if (!window.BroadcastChannel) {
+  console.error("BroadcastChannel not supported in this browser.");
+  return;
+}
+
+// ___Begin visible code snippet___
+
+const channel = new BroadcastChannel("my-channel");
+console.log("Broadcast channel created.");
+
+let count = 0;
+const interval = setInterval(() => {
+  if (count++ < 30) {
+    console.log(`Sending message ${count}...`);
+    channel.postMessage({
+      message: `Message ${count}`,
+      timestamp: new Date().toUTCString(),
+    });
+  } else {
+    console.log("Stopped sending messages...");
+    clearInterval(interval);
+    channel.close();
+  }
+}, 2000);
