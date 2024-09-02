c9c8de46319ccc0fdc17c0337653b72cf9ba1e0c
Andy <onsubmit@outlook.com>
2024-09-02 12:55:17 -0700

    Fix CommonJS import issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,41 @@
-import starlight from "@astrojs/starlight";
-import { defineConfig } from "astro/config";
-
-import react from "@astrojs/react";
+import react from '@astrojs/react';
+import starlight from '@astrojs/starlight';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://onsubmit.github.io",
-  base: "web-apis",
+  site: 'https://onsubmit.github.io',
+  base: 'web-apis',
   integrations: [
     starlight({
-      title: "Web APIs",
+      title: 'Web APIs',
       social: {
-        github: "https://github.com/onsubmit/web-apis",
+        github: 'https://github.com/onsubmit/web-apis',
       },
       editLink: {
-        baseUrl: "https://github.com/onsubmit/web-apis/edit/main",
+        baseUrl: 'https://github.com/onsubmit/web-apis/edit/main',
       },
-      customCss: ["./src/styles/custom.css"],
+      customCss: ['./src/styles/custom.css'],
       sidebar: [
         {
-          label: "Specifications",
+          label: 'Specifications',
           autogenerate: {
-            directory: "js",
+            directory: 'js',
           },
         },
       ],
     }),
     react(),
   ],
+  // https://github.com/withastro/astro/issues/7629
+  vite: {
+    ssr: {
+      noExternal: [
+        '@mui/joy',
+        '@mui/styled-engine',
+        '@mui/system',
+        '@mui/utils',
+      ],
+    },
+  },
 });
